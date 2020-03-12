aee9b51b7ba6ef82e55d0c9940478fbb90164300
Triumph <RyanMoe@protonmail.com>
2020-03-11 18:38:50 -0700

    Rewrote views so they do something. Added templates for index and thread topics.

--- rychat/views.py
+++ rychat/views.py
@@ -1,6 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
+from .models import Thread, Reply
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world")
+    thread_index = Thread.objects.order_by('-date')
+    context = {
+        'thread_index' : thread_index,
+    }
+    return render(request, 'rychat/index.html', context)
+
+def thread(request, thread_id):
+    thread_topic = get_object_or_404(Thread, pk=thread_id)
+    thread_replies = Thread.reply_set.order_by('date')
+    context = {
+        'thread_topic'      : thread_topic,
+        'thread_replies'    : thread_replies,
+    }
+    return render(request, 'rychat/topic.html', context)
