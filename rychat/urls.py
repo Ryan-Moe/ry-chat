86f2269e8b469b9f77ac0dba40d2b5921fa7c4aa
Triumph <RyanMoe@protonmail.com>
2020-03-12 23:40:25 -0700

    Added posting functionality to thread template. Added view to handle posts.

--- rychat/urls.py
+++ rychat/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'rychat'
 urlpatterns = [
    path('', views.index, name='index'),
-   path('topic/<int:thread_id>/', views.thread, name='topic')
+   path('topic/<int:thread_id>/', views.thread, name='topic'),
+   path('topic/<int:thread_id>/post/', views.post_reply, name='post'),
 ]
--- rychat/views.py
+++ rychat/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.utils import timezone
+from django.urls import reverse
 from .models import Thread, Reply
 
 # Create your views here.
@@ -12,9 +14,21 @@ def index(request):
 
 def thread(request, thread_id):
     thread_topic = get_object_or_404(Thread, pk=thread_id)
-    thread_replies = Thread.reply_set.order_by('date')
+    thread_replies = thread_topic.reply_set.order_by('date')
     context = {
         'thread_topic'      : thread_topic,
         'thread_replies'    : thread_replies,
     }
     return render(request, 'rychat/topic.html', context)
+
+def post_reply(request, thread_id):
+    thread_topic = get_object_or_404(Thread, pk=thread_id)
+    reply_text = request.POST['message']
+    reply_text = reply_text[:1024]
+
+    r = Reply(thread=Thread.objects.get(pk=thread_id),
+        text=reply_text,
+        date=timezone.now(),
+        author="RyAdmin")
+    r.save()
+    return HttpResponseRedirect(reverse('rychat:topic', args=(thread_id,)))
