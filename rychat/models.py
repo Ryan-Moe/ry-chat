04c899118aa442f1956af8e0329a453a210f97f3
Triumph <RyanMoe@protonmail.com>
2020-06-03 23:24:00 -0700

    +Delete end-user post/tread functionality

--- rychat/models.py
+++ rychat/models.py
@@ -2,8 +2,6 @@ from django.db import models
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
 
-#TODO: convert "author" fields to foreign key of user
-
 # Create your models here.
 class Thread(models.Model):
     title = models.CharField(max_length = 256)
